4ef7adaaf16adb8491d6a42d5eb8a1838be5159f
Eli <elizatelkamp@gmail.com>
2024-04-08 22:51:06 -0400

    Added win condition

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private TMP_Text distanceText;
+
+    private float distance = 0;
+    private bool isPlaying = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(UpdateDistance());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator UpdateDistance()
+    {
+        while (isPlaying)
+        {
+            distance += 1f;
+            distanceText.text = distance.ToString("F0");
+            CheckWin();
+            yield return new WaitForSeconds(0.2f);
+        }    
+    }
+
+    void CheckWin()
+    {
+        if (distance >= 1000)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 }
